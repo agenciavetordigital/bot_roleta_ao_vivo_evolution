a984886ce4d644f077ce654a314bd5d6b44c1404
Diogo <agenciavetordigital@gmail.com>
2025-09-29 00:05:59 -0300

    Update roulette_monitor.py

--- roulette_monitor.py
+++ roulette_monitor.py
@@ -34,9 +34,9 @@ INTERVALO_VERIFICACAO_API = 5
 MAX_MARTINGALES = 2
 
 # --- CONFIGURAÇÕES DE ESTRATÉGIA ---
-GATILHO_ATRASO_DUZIA = 8
+GATILHO_ATRASO_DUZIA = 7
 NUMEROS_PARA_ANALISE = 50
-GATILHO_CONFIANCA_IA_DUZIAS = 0.55
+GATILHO_CONFIANCA_IA_DUZIAS = 0.53
 GATILHO_CONFIANCA_IA_TOP5 = 0.27
 SEQUENCE_LENGTH_IA_DUZIAS = 15
 SEQUENCE_LENGTH_IA_NUMEROS = 20
@@ -406,4 +406,5 @@ if __name__ == '__main__':
     try: asyncio.run(supervisor())
     except KeyboardInterrupt: logging.info("Bot encerrado manualmente.")
     except Exception as e: logging.critical(f"Erro fatal no supervisor: {e}")
+
 
